4071c500f27cbc0a2256d492275a25af92692b0b
Samuel Ramos <samuelvanguarda@gmail.com>
2024-02-15 16:41:19 -0300

    feat: HelloWorld Component

---
+++ frontend/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/exercicio-02/vite-project/src/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import ModuleDetails from "./ModuleDetails";
+import Title from "./Title";
+
+function HelloWorld() {
+  return (
+    <>
+      <Title />
+      <ModuleDetails />
+    </>
+  )
+}
+
+export default HelloWorld;
---
+++ frontend/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/exercicio-02/vite-project/src/components/ModuleDetails.tsx
@@ -0,0 +1,14 @@
+function ModuleDetails() {
+  return (
+    <>
+      <ul>
+        <li>Fundamentos</li>
+        <li>Frontend</li>
+        <li>Backend</li>
+        <li>Ciência da Computação</li>
+      </ul>
+    </>
+  )
+}
+
+export default ModuleDetails;
